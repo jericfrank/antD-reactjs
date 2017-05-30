62c368f5b19c04d0f36b352a0e1a65a7a8137e23
Jeric Frank Sasil <jericfrank@gmail.com>
2017-05-30 16:09:47 +0800

    layout

---
+++ app/containers/App/css.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Logo = styled.div`
+  .logo {
+    width: 120px;
+    height: 31px;
+    background: #333;
+    border-radius: 6px;
+    margin: 16px 24px 16px 0;
+    float: left;
+  }
+`;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,9 +15,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { makeSelectAuthenticated, makeSelectCurrentUser } from './selectors';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-import Header from 'components/Header';
+import { Logo } from './css';
+
+import { makeSelectAuthenticated, makeSelectCurrentUser } from './selectors';
 
 class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -25,23 +28,52 @@ class App extends React.PureComponent { // eslint-disable-line react/prefer-stat
     children: React.PropTypes.node,
   };
 
-  renderHeader(){
-    const { authenticated, currentUser } = this.props;
+  constructor() {
+    super();
 
-    if ( authenticated ) {
-      return (
-        <Header user={currentUser.user}/>
-      );
-    }
-
-    return '';
+    this.renderLayout  = this.renderLayout.bind( this );
   }
 
+  renderLayout(){
+    return (
+      <Layout>
+        <Header style={{ position: 'fixed', width: '100%' }}>
+          <Logo>
+            <div className="logo"/>
+          </Logo>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px', marginTop: 64 }}>
+          <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 650 }}>
+            {React.Children.toArray(this.props.children)}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2016 Created by Ant UED
+        </Footer>
+      </Layout>
+    );
+  }
   render() {
+    const { authenticated } = this.props;
+
     return (
       <div>
-        {this.renderHeader()}
-        {React.Children.toArray(this.props.children)}
+        { ( authenticated ) ? this.renderLayout() : React.Children.toArray(this.props.children) }
       </div>
     );
   }
